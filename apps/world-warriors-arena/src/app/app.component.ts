5089c27e7bd5c9a681ccdfaedce890baafa5708e
Eric Draper <edraper@qualiti.ai>
2021-08-12 19:59:22 -0600

    First Commit

---
+++ apps/world-warriors-arena/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'world-warriors-arena-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'world-warriors-arena';
+}
